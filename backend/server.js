97dd726073acc3dd409c8f9fe8ab71a5e914ced5
omsrg <siregar.om@gmail.com>
2022-02-27 18:04:22 +0700

    fix typo

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend
-if (process.env.NODE.ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 	app.get('*', (req, res) =>
